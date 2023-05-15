bb86c98769f7ecfccb67d58e87cabde930c32797
mld-0 <mld.0@protonmail.com>
2023-05-15 21:53:34 +1000

    cleanup, report runtime %s

--- cpu-bench-v1/_run.zsh
+++ cpu-bench-v1/_run.zsh
@@ -4,42 +4,74 @@ set -o errexit   # abort on nonzero exitstatus
 set -o nounset   # abort on unbound variable
 set -o pipefail  # don't hide errors within pipes
 
+microtime() { python3 -c 'import time; print(int(time.time_ns()/1000000))' }
+
+run_benchmark() {
+    local name="$1"
+    local output="$(./$name)"
+	local runtime=$(echo "$output" | awk '{print $(NF-1)}')
+	runtimes+=( "$runtime" )
+	total_runtime=( $( perl -E "say $total_runtime + $runtime" ) )
+	outputs+=( "$output" )
+}
+
+funcs=( integer-arithemetic float-arithmetic memory-access matrix-multiply parallel-float parallel-sin basic-instructions random-access )
+
 gpp=g++
 gpp_args=( -std=c++17 -O3 )
-echo "gpp=($gpp), gpp_args=(${gpp_args[@]})"
 
-startTime=$( python3 -c 'import time; print(int(time.time_ns()/1000000))' )
-$gpp ${gpp_args[@]} -o integer-arithemetic integer-arithemetic.cpp
-$gpp ${gpp_args[@]} -o float-arithmetic float-arithmetic.cpp
-$gpp ${gpp_args[@]} -o memory-access memory-access.cpp
-$gpp ${gpp_args[@]} -o matrix-multiply matrix-multiply.cpp
-$gpp ${gpp_args[@]} -o parallel-float parallel-float.cpp
-$gpp ${gpp_args[@]} -o parallel-sin parallel-sin.cpp
-$gpp ${gpp_args[@]} -o basic-instructions basic-instructions.cpp
-$gpp ${gpp_args[@]} -o random-access random-access.cpp
-endTime=$( python3 -c 'import time; print(int(time.time_ns()/1000000))' )
-elapsedTime=$( perl -E "say $endTime - $startTime" )
-echo "build time: $elapsedTime ms"
+runtimes=()
+outputs=()
+total_runtime=0
 
-startTime=$( python3 -c 'import time; print(int(time.time_ns()/1000000))' )
-./integer-arithemetic
-./float-arithmetic
-./memory-access
-./matrix-multiply
-./parallel-float
-./parallel-sin
-./basic-instructions
-./random-access
-endTime=$( python3 -c 'import time; print(int(time.time_ns()/1000000))' )
-elapsedTime=$( perl -E "say $endTime - $startTime" )
-echo "run time: $elapsedTime ms"
+main() 
+{
+	#	Build Binaries
+	echo "build: $gpp ${gpp_args[@]}"
+	startTime=$( microtime )
+	for func in "${funcs[@]}"; do
+		$gpp ${gpp_args[@]} -o "$func" "$func.cpp"
+	done
+	endTime=$( microtime )
+	elapsedTime=$( perl -E "say $endTime - $startTime" )
+	echo "build time: $elapsedTime ms"
 
-rm integer-arithemetic
-rm memory-access
-rm matrix-multiply
-rm float-arithmetic
-rm parallel-float
-rm parallel-sin
-rm basic-instructions
-rm random-access
+
+	#	Run Binaries
+	startTime=$( microtime )
+	for f in "${funcs[@]}"; do
+		run_benchmark "$f"
+	done
+	endTime=$( microtime )
+	elapsedTime=$( perl -E "say $endTime - $startTime" )
+	runtime_overhead=$( perl -E "printf('%.1f', $elapsedTime - $total_runtime/1000.0)" )
+	echo "run time: $elapsedTime ms (overhead: $runtime_overhead ms)"
+
+
+	#	Delete binaries
+	for func in "${funcs[@]}"; do
+		rm "$func"
+	done
+
+
+	#	Report percentage runtimes:
+	i=1
+	runtime_percents=()
+	for runtime in "${runtimes[@]}"; do
+		runtime_percents+=( "$( perl -E "printf('%.1f', ($runtime / $total_runtime) * 100.0)" )" )
+	done
+	while [[ $i -le ${#outputs[@]} ]]; do
+		echo -n "${outputs[$i]}"
+		echo -n " "
+		echo "(${runtime_percents[$i]}%)"
+		((i++))
+	done
+
+
+	#	Finish
+	echo "DONE"
+	exit 0
+}	
+
+main "$@"
 
